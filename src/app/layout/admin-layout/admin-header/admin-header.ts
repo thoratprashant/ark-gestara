735b1b5dcc36e94a8c467b1e3f9aa0c64b4c88fe
thoratprashant <prashant.thorat@arkenea.com>
2026-08-31 16:21:07 +0530

    UI

--- src/app/layout/admin-layout/admin-header/admin-header.ts
+++ src/app/layout/admin-layout/admin-header/admin-header.ts
@@ -1,10 +1,15 @@
 import { Component, output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-admin-header',
+  imports: [MatIconModule, MatMenuModule],
   templateUrl: './admin-header.html',
   styleUrl: './admin-header.scss',
 })
 export class AdminHeader {
   readonly menuClick = output<void>();
+  readonly profileClick = output<void>();
+  readonly logoutClick = output<void>();
 }
